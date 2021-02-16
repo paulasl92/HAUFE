dd8b10636197df34cdc041bc09639ab1c3b32d21
Paula Salamone Lacunza <paulasl92@gmail.com>
2021-02-16 21:12:57 +0100

    add 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import NavBar from "./component/Navbar";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/Signup";
-import Home from "./pages/Home"
-import { UserRoute } from "./component/privateRoute"
+import Home from "./pages/Home";
+import { UserRoute } from "./component/privateRoute";
+import NotFoundPage from "./component/NotFoundPage";
 
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Route path="/signup" exact>
-        <SignUp />
-      </Route>
-      <Route path="/" exact>
-        <SignIn />
-      </Route>
-      <UserRoute path="/home">
-        <Home />
-      </UserRoute>
+      <Switch>
+        <Route path="/signup" exact>
+          <SignUp />
+        </Route>
+        <Route path="/" exact>
+          <SignIn />
+        </Route>
+        <UserRoute path="/home">
+          <Home />
+        </UserRoute>
+        <Route component={NotFoundPage} />
+      </Switch>
     </Router>
   );
 }
---
+++ client/src/component/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import error from '../util/images/404message.jpg';
+
+const NotFoundPage = () => {
+    return (
+      <div>
+        <h1>Sorry! Rick is fighting with Morty, and they broke the app... again</h1>
+        <img src={error} alt="404 Image Error"/>
+      </div>
+    )
+  }
+
+export default NotFoundPage;
