1ffdd765cce51161c477bcfd335b6ce26cfb6dc4
Paula Salamone Lacunza <paulasl92@gmail.com>
2021-02-18 02:33:17 +0100

    move mongoose config

--- api/app.js
+++ api/app.js
@@ -1,18 +1,5 @@
-//Import the mongoose module
-var mongoose = require('mongoose');
-
-//Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-var key = require('./keys');
+const { connect } = require("./db/mongoose");
+connect();
 var authRouter = require('./routes/auth');
 var rAndM = require('./routes/rickAndMorty');
 
